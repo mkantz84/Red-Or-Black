221e5de30ae0bff4b4f1204dee71f01471e83e8d
kantz <kantz@LAPTOP-AE55S8JL>
2017-04-16 02:13:14 +0300

    add comments

--- Logic.js
+++ Logic.js
@@ -1,3 +1,4 @@
+
 const GAME_LEVELS = 5;
 const TIME_SET = 1000;
 const PC_MIN_WIDTH = 480;
@@ -13,11 +14,22 @@ var cards =
      "Card_Red_04.png", "Card_Red_05.png", "Card_Red_06.png"];
 
 $(document).ready(function () {
+
     CheckSizeAndFix();
-    $('.loader').delay(500).fadeTo(0, 1, function () {
+
+    //*** delay loader a bit (just to be seen): 
+    $('.loader').delay(TIME_SET / 2).fadeTo(0, 1, function () {
         $('.loader').hide();
     });
+
+    //*** catch mouse down on any card button to change the tranform and make the card spin:
+    $('.clrBtn').bind("mousedown touchstart", (function () {
+        $('#selectedCard').css("transform", "");
+    }));
+
+    //*** catch mouse up on any card button and make animation:
     $('.clrBtn').bind("mouseup touchend", (function () {
+        // disable auto play:
         if (clicks == 0) {
             $('#autoPlay').prop('disabled', true);
             $('#autoPlay').css('opacity', 0.7);
@@ -30,30 +42,29 @@ $(document).ready(function () {
         $('#selectedCard').attr("src", "images/cards/" + card);
         $('#selectedCard').css("visibility", "visible");
         $('#selectedCard').css("transform", "rotateY(360deg)");
-        $(".clrBtn").prop('disabled', true);
+        $(this).prop('disabled', true);
     }));
 
-    $('.clrBtn').bind("mousedown touchstart", (function () {
-        $('#selectedCard').css("transform", "");
-    }));
-
+    //*** catch mouse down of a card, change visibility and disable other card:
     $('#redBtn').bind("mousedown touchstart", (function () {
-        $('#redBtn').attr("src", "images/Red_Selected.png");
+        $(this).attr("src", "images/Red_Selected.png");
         $('#blackBtn').attr('src', 'images/Black_disabled.png');
-    }));
-
-    $('#redBtn').bind("mouseup touchend", (function () {
-        GameLogic("red");
-        $('#redBtn').delay(TIME_SET * 2).fadeTo(0, 1, function () {
-            $('#redBtn').attr('src', 'images/Red_idle.png');
-            $('#blackBtn').attr('src', 'images/Black_idle.png');
-            $(".clrBtn").prop('disabled', false);
-        });
     }));
 
     $('#blackBtn').bind("mousedown touchstart", (function () {
         $('#blackBtn').attr("src", "images/Black_Selected.png");
         $('#redBtn').attr('src', 'images/Red_disabled.png');
+    }));
+
+    //*** catch mouse up of a card, start logic and show card changing animation:
+    $('#redBtn').bind("mouseup touchend", (function () {
+        GameLogic("red");
+        // a manipulation of a fake fade to activate a function after a delay:
+        $('#redBtn').delay(TIME_SET * 2).fadeTo(0, 1, function () {
+            $('#redBtn').attr('src', 'images/Red_idle.png');
+            $('#blackBtn').attr('src', 'images/Black_idle.png');
+            $(".clrBtn").prop('disabled', false);
+        });
     }));
 
     $('#blackBtn').bind("mouseup touchend", (function () {
@@ -65,51 +76,55 @@ $(document).ready(function () {
         });
     }));
 
+    //*** hiding the help image:
     $('#hlpImg').on('click', function () {
         $(this).hide();
     });
 
+    //*** show/hide help image (always show):
     $('#inf').on('click', function () {
         $('#hlpImg').toggle();
     });
 
+    //*** a manipulation to fix image competability for all browsers:
     $(window).resize(function () {
         CheckSizeAndFix();
     });
 
+    //*** auto play logic
     $('#autoPlay').on('click', function () {
         AutoPlay();
 
-        //*** a solution for automtic values climbing the steps meter (automatic winning):
+        //** a solution for automtic values climbing the steps meter (automatic winning):
         //AutoWin();        
     });
 
+    //*** opening the rules button and show info:
     $('.show-hide-rules').on('click', function () {
         if ($('.rules-info').css('display') == 'none') {
             $('.show-hide-rules .plus-minus').html('[-]');
-            $('.rules-info').show();
+            $('.rules-info').slideDown();
         }
         else {
             $('.show-hide-rules .plus-minus').html('[+]');
             $('.rules-info').slideUp();
         }
-        window.scrollTo(0, document.body.clientHeight);
     });
 
+    //*** opening the terms & condition button and show info:
     $('.show-hide-terms').on('click', function () {
         if ($('.terms-info').css('display') == 'none') {
             $('.show-hide-terms .plus-minus').html('[-]');
-            $('.terms-info').show();
+            $('.terms-info').slideDown();
         }
         else {
             $('.show-hide-terms .plus-minus').html('[+]');
             $('.terms-info').slideUp();
         }
-        window.scrollTo(0, document.body.clientHeight);
     });
 });
 
-//*** a manipulation to fix competability for all browsers:
+//*** change images src in html via js for FF and Edge competability:
 function CheckSizeAndFix() {
     if ($(window).width() < PC_MIN_WIDTH) {
         if ($('.main-img img').attr('src').includes('PC')) {
@@ -127,12 +142,16 @@ function CheckSizeAndFix() {
     }
 }
 
+//*** automating the card button choosing:
 function AutoPlay() {
+    // picking between red or black randomaly:
     var colors = ["red", "black"];
     var color = colors[Math.floor(Math.random() * colors.length)];
     color += 'Btn';
+    // triggering the click on the card we picked:
     $('#' + color).trigger('mousedown');
     $('#' + color).trigger('mouseup');
+    // disabling the auto play button until the auto play logic and animation has done: 
     $('#autoPlay').delay(TIME_SET * 2).fadeTo(0, 1, function () {
         $(this).prop('disabled', false);
         $(this).css('opacity', 'initial');
@@ -140,19 +159,22 @@ function AutoPlay() {
     clicks--;
 }
 
+//*** understanding the guess and acting accordingly:
 function GameLogic(color) {
+    // guess right:
     if (card.toLowerCase().includes(color)) {
         winCount++;
         $('#result').attr("src", "images/Card_V.png");
-        $('#result').fadeIn(800);
-        $('.card-container').delay(TIME_SET).fadeOut(800);
+        $('#result').fadeIn(TIME_SET);
+        $('.card-container').delay(TIME_SET).fadeOut(TIME_SET);
         HandleWins(winCount);
     }
+        // guess wrong:
     else {
         lives--;
         $('#result').attr("src", "images/Card_X.png");
-        $('#result').fadeIn(800);
-        $('.card-container').delay(TIME_SET).fadeOut(800);
+        $('#result').fadeIn(TIME_SET);
+        $('.card-container').delay(TIME_SET).fadeOut(TIME_SET);
         HandleLives();
     }
 }
@@ -163,16 +185,18 @@ function HandleWins(wins) {
         case 3:
         case 4:
         case 5:
+            //change the level image for all devices:
             $('#level').delay(TIME_SET).fadeTo(0, 1, function () {
                 $(window).width() < PC_MIN_WIDTH ?
                 $(this).attr("src", "images/RED_or_BLACK_Mobile_step_0" + wins + ".png") :
                 $(this).attr("src", "images/RED_or_BLACK_PC_step_0" + wins + ".png");
                 $(this).css('display', 'inline-table')
             }).fadeIn(TIME_SET, function () {
+                // if its the last level, show winning modal:
                 if ($('#level').attr('src').includes('5')) {
                     $('.end-game-modal div').html
                         ('CONGRATULATIONS!!<br /><input id="plyBtn" onclick="StartNewGame()" type="button" name="name" value="Play again" />');
-                    $('.end-game-modal').fadeIn(500);
+                    $('.end-game-modal').delay(TIME_SET).fadeIn(TIME_SET / 2);
                 }
             });
             break;
@@ -181,6 +205,7 @@ function HandleWins(wins) {
     }
 }
 
+//*** deal with the lives image and loose modal:
 function HandleLives() {
     switch (lives) {
         case 1:
@@ -194,14 +219,13 @@ function HandleLives() {
             });
             $('.end-game-modal div').html
                 ('GAME OVER...<br /><input id="plyBtn" onclick="StartNewGame()" type="button" name="name" value="Play again" />');
-            $('.end-game-modal').delay(TIME_SET).fadeIn(500);
-
-
+            $('.end-game-modal').delay(TIME_SET * 2).fadeIn(TIME_SET / 2);
         default:
             break;
     }
 }
 
+//*** reset all settings and variables to start a new and clean game:
 function StartNewGame() {
     winCount = 1;
     lives = 2;
@@ -217,6 +241,7 @@ function StartNewGame() {
     $('.end-game-modal').hide();
 }
 
+// a none usable function for another logic for auto play:
 function AutoWin() {
     $('#inf').prop('disabled', true);
     $('#autoPlay').prop('disabled', true);
